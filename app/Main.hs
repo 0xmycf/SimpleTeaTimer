0fd65ba1a8b31d5b6aca1c6c9dd8ba5d452f7a04
rlDruDo <ichundeli@gmail.com>
2021-11-25 14:43:25 +0100

    Init refactored branch

--- app/Main.hs
+++ app/Main.hs
@@ -19,14 +19,15 @@ data Tea
 waitConstant :: Int
 waitConstant = 1000000
 
+getTea = getInput >>= mapStringToTea
+
 main :: IO ()
-main = teapot (0, toLowerCase getInput)
+main = do
+  tea <- getTea
+  teapot (0, tea)
 
-teapot :: (Int, IO String) -> IO ()
-teapot (x, teaString) = teaString >>= mapTeaToTime . mapStringToTea x >>= brewTheTea >>= teapot2
-
-teapot2 :: (Int, Tea) -> IO ()
-teapot2 (x, tea) = moreTeaValidation getLine >> mapTeaToTime (return tea, x) >>= brewTheTea >>= teapot2
+teapot :: (Int, Tea) -> IO ()
+teapot (i, tea) = mapTeaToTime tea i >>= brewTheTea >>= teapot
 
 toLowerCase :: IO String -> IO String
 toLowerCase = (fmap . map) toLower
@@ -36,32 +37,31 @@ getInput = do
   putStrLn "Enter the tea."
   getLine
 
-mapStringToTea :: Int -> String -> (IO Tea, Int)
-mapStringToTea x "white" = (return White, x)
-mapStringToTea x "weiß" = (return White, x)
-mapStringToTea x "green" = (return Green, x)
-mapStringToTea x "grün" = (return Green, x)
-mapStringToTea x "yellow" = (return Yellow, x)
-mapStringToTea x "gelb" = (return Yellow, x)
-mapStringToTea x "oolongstrip" = (return OolongStrip, x)
-mapStringToTea x "oolongs" = (return OolongStrip, x)
-mapStringToTea x "oolong" = (return OolongStrip, x)
-mapStringToTea x "oolongball" = (return OolongBall, x)
-mapStringToTea x "oolongb" = (return OolongBall, x)
-mapStringToTea x "puerhripe" = (return PuerhRipe, x)
-mapStringToTea x "puerhr" = (return PuerhRipe, x)
-mapStringToTea x "puerhstrip" = (return PuerhStrip, x)
-mapStringToTea x "puerhs" = (return PuerhStrip, x)
-mapStringToTea x "black" = (return Black, x)
-mapStringToTea x "schwarz" = (return Black, x)
-mapStringToTea _ _ = (return NoTea, 0)
+mapStringToTea :: String -> IO Tea
+mapStringToTea "white"        = return White
+mapStringToTea "weiß"         = return White
+mapStringToTea "green"        = return Green
+mapStringToTea "grün"         = return Green
+mapStringToTea "yellow"       = return Yellow
+mapStringToTea "gelb"         = return Yellow
+mapStringToTea "oolongstrip"  = return OolongStrip
+mapStringToTea "oolongs"      = return OolongStrip
+mapStringToTea "oolong"       = return OolongStrip
+mapStringToTea "oolongball"   = return OolongBall
+mapStringToTea "oolongb"      = return OolongBall
+mapStringToTea "puerhripe"    = return PuerhRipe
+mapStringToTea "puerhr"       = return PuerhRipe
+mapStringToTea "puerhstrip"   = return PuerhStrip
+mapStringToTea "puerhs"       = return PuerhStrip
+mapStringToTea "black"        = return Black
+mapStringToTea "schwarz"      = return Black
+mapStringToTea _              = return NoTea
 
-mapTeaToTime :: (IO Tea, Int) -> IO (Int, Tea)
-mapTeaToTime (tea, oldTime) = do
-  val <- tea
-  case val of
+mapTeaToTime :: Tea -> Int -> IO (Int, Tea)
+mapTeaToTime tea i = do
+  case tea of
     NoTea -> goodBye "Please enter a valid tea you doofus"
-    _ -> return (calcInfusion val oldTime, val)
+    _ -> return (calcInfusion tea i, tea)
 
 intToString :: Int -> IO String
 intToString = return . show
@@ -73,40 +73,40 @@ goodBye msg = do
 
 brewTheTea :: (Int, Tea) -> IO (Int, Tea)
 brewTheTea (wait, tea) = do
-  putStrLn $ "Brewing the tea for " ++ show wait ++ " seconds!"
+  moreTeaValidation $ show wait
   threadDelay $ wait * waitConstant
   putStrLn "Enjoy!"
   return (wait, tea)
 
 calcInfusion :: Tea -> Int -> Int
-calcInfusion White 0 = 20
-calcInfusion Green 0 = 15
-calcInfusion Yellow 0 = 15
-calcInfusion OolongStrip 0 = 20
-calcInfusion OolongBall 0 = 25
-calcInfusion PuerhRipe 0 = 10
-calcInfusion PuerhStrip 0 = 10
-calcInfusion Black 0 = 10
-calcInfusion NoTea 0 = 0
-calcInfusion White x = x + 10
-calcInfusion Green x = x + 3
-calcInfusion Yellow x = x + 5
-calcInfusion OolongStrip x = x + 5
-calcInfusion OolongBall x = x + 5
-calcInfusion PuerhRipe x = x + 5
-calcInfusion PuerhStrip x = x + 3
-calcInfusion Black x = x + 5
-calcInfusion NoTea x = x + 0
+calcInfusion White 0        = 20
+calcInfusion Green 0        = 15
+calcInfusion Yellow 0       = 15
+calcInfusion OolongStrip 0  = 20
+calcInfusion OolongBall 0   = 25
+calcInfusion PuerhRipe 0    = 10
+calcInfusion PuerhStrip 0   = 10
+calcInfusion Black 0        = 10
+calcInfusion NoTea 0        = 0
+calcInfusion White x        = x + 10
+calcInfusion Green x        = x + 3
+calcInfusion Yellow x       = x + 5
+calcInfusion OolongStrip x  = x + 5
+calcInfusion OolongBall x   = x + 5
+calcInfusion PuerhRipe x    = x + 5
+calcInfusion PuerhStrip x   = x + 3
+calcInfusion Black x        = x + 5
+calcInfusion NoTea x        = x + 0
 
-moreTeaValidation :: IO String -> IO ()
-moreTeaValidation x = do
-  putStrLn "Do you want to continue your session?"
-  val <- x
-  let byebye = goodBye "I hope you enjoyed your tea!"
+moreTeaValidation :: String -> IO ()
+moreTeaValidation seconds = do
+  putStrLn $ "Continue brewing for " ++ seconds ++ " seconds?"
+  val <- getLine
   case val of
-    "yes" -> putStrLn "Then lets continue..."
-    "y" -> putStrLn "Then lets continue..."
-    "yea" -> putStrLn "Then lets continue..."
-    "yeah" -> putStrLn "Then lets continue..."
-    "ja" -> putStrLn "Then lets continue..."
-    _ -> byebye
+    "yes"     -> putStrLn $  "Brewing the tea for " ++ seconds ++ " seconds!"
+    "y"       -> putStrLn $ "Brewing the tea for " ++ seconds ++ " seconds!"
+    "yea"     -> putStrLn $ "Brewing the tea for " ++ seconds ++ " seconds!"
+    "yeah"    -> putStrLn $ "Brewing the tea for " ++ seconds ++ " seconds!"
+    "ja"      -> putStrLn $ "Brewing the tea for " ++ seconds ++ " seconds!"
+    "j"       -> putStrLn $ "Brewing the tea for " ++ seconds ++ " seconds!"
+    _         -> goodBye "I hope you enjoyed your tea!"
